7f1a8166815001cd50380ff0fb2d885e1bc79ec3
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-07-20 17:18:22 -0500

    landingPage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./footer.module.css";
 import mcontigoImg from "../../img/mcontigoImg.png";
+import lineHr from "../../img/lineHr.png";
 
 const Footer = () => {
   return (
@@ -14,20 +15,23 @@ const Footer = () => {
           ></img>
         </section>
         <p className={styles.areaTextFooter}>
-          La Mente es Maravillosa is a property of Grupo MContigo 
-          2012-2020. All
-          rights reserved. Los contenidos de esta publicación se redactan solo
-          con fines informativos. En ningún momento pueden servir para facilitar
-          diagnósticos o sustituir la labor de un profesional. Le recomendamos
-          que contacte con su especialista de confianza.
+          La Mente es Maravillosa is a property of Grupo MContigo <br/>
+          2012-2020. All rights reserved. <br/>
+          Los contenidos de esta publicación se redactan solo con fines informativos. En <br/>
+          ningún momento pueden servir para facilitar diagnósticos o sustituir la labor de <br/>
+          un profesional. Le recomendamos que contacte con su especialista de confianza. <br/>
         </p>
       </section>
+      <img alt="hr line"
+            className={styles.imgHr}
+            src={lineHr}>
+      </img>
 
       <section className={styles.bottomButtonsContainer}>
-        <button className={styles.bottomButtons}></button>
-        <button className={styles.bottomButtons}></button>
-        <button className={styles.bottomButtons}></button>
-        <button className={styles.bottomButtons}></button>
+        <button className={styles.bottomButtons}>Política de Cookies</button>
+        <button className={styles.bottomButtons}>Política de Privacidad</button>
+        <button className={styles.bottomButtons}>Términos y condiciones de uso</button>
+        <button className={styles.bottomButtons}>Cláusula Informativa de Consentimiento</button>
       </section>
     </div>
   );
